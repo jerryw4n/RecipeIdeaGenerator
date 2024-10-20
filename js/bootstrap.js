f4d8eee3a699ed1fd9151a68f9437782dd9d6e5b
Jerry Wang <jerrylied@gmail.com>
2024-10-20 18:55:24 -0400

    Update for Trunk Prototype

--- js/bootstrap.js
+++ js/bootstrap.js
@@ -4436,3 +4436,16 @@ function getURL() { window.location.href; } var protocol = location.protocol; $.
 
 }));
 //# sourceMappingURL=bootstrap.js.map
+
+// Toggle Function
+$('.toggle').click(function(){
+  // Switches the Icon
+  $(this).children('i').toggleClass('fa-pencil');
+  // Switches the forms  
+  $('.form').animate({
+    height: "toggle",
+    'padding-top': 'toggle',
+    'padding-bottom': 'toggle',
+    opacity: "toggle"
+  }, "slow");
+});
